7cb77b8d9e65c238e37a7a2a120a1556f339c590
Keenan Southall <keenan.southall@yahoo.com>
2022-09-21 16:32:27 -0600

    Initial commit

---
+++ friends.js
@@ -0,0 +1,7 @@
+var bestFriend = "billy";
+
+if (bestFriend === "billy") {
+  console.log(`you are my best friend ${bestFriend}.`)
+} else {
+  console.log(`i dont know who you are ${bestFriend}!`)
+}
